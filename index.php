45305a202e8452990af060feb2276d4b470f63cc
joaoHass <smartpocketbr@gmail.com>
2022-01-06 11:29:42 -0300

    added GTM

--- index.php
+++ index.php
@@ -60,9 +60,35 @@
       as="style"
       onload="this.onload=null;this.rel='stylesheet'"
     />
+
+    <!-- Google Tag Manager -->
+    <script>
+      ;(function (w, d, s, l, i) {
+        w[l] = w[l] || []
+        w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" })
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l != "dataLayer" ? "&l=" + l : ""
+        j.async = true
+        j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl
+        f.parentNode.insertBefore(j, f)
+      })(window, document, "script", "dataLayer", "GTM-NNJSG4Z")
+    </script>
+    <!-- End Google Tag Manager -->
   </head>
 
   <body>
+    <!-- Google Tag Manager (noscript) -->
+    <noscript
+      ><iframe
+        src="https://www.googletagmanager.com/ns.html?id=GTM-NNJSG4Z"
+        height="0"
+        width="0"
+        style="display: none; visibility: hidden"
+      ></iframe
+    ></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+
     <section class="banner">
       <img src="./img/marcelo.png" alt="" class="img-marcelo" />
       <div class="row">
